8291d626fcab9c8578e62221baf511a5f239fbfd
flightlogscan <flightlogscan@flightlogscan.com>
2025-07-08 19:42:04 -0700

    test logo change

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,22 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          src="/scramble-logo.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={309}
+          height={125}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            test{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            fdas.
           </li>
         </ol>
 
